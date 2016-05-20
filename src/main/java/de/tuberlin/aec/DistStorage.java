a4ea9e6cae5efbcc07b8c77adcfdf196f1ae39f3
joh-mue <yesiamkeen@gmail.com>
2016-05-20 16:33:03 +0200

    inital mvn setup

---
+++ src/main/java/de/tuberlin/aec/DistStorage.java
@@ -0,0 +1,13 @@
+package de.tuberlin.aec;
+
+/**
+ * Hello world!
+ *
+ */
+public class DistStorage 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
