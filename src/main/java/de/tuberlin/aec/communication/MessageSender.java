6906f8463d5b8dab69e35ccb6b91a8012be643d3
joh-mue <yesiamkeen@gmail.com>
2016-05-24 22:28:12 +0200

    added MessageSender

---
+++ src/main/java/de/tuberlin/aec/communication/MessageSender.java
@@ -0,0 +1,32 @@
+package de.tuberlin.aec.communication;
+
+import de.tub.ise.hermes.Request;
+import de.tub.ise.hermes.Sender;
+import de.tuberlin.aec.message.SyncWriteCommitMessage;
+import de.tuberlin.aec.message.SyncWriteSuggestionMessage;
+
+/**
+ * Created by joh-mue on 24/05/16.
+ */
+public class MessageSender {
+    private void sendMessage(String host, int port, Request request) {
+        Sender sender = new Sender(host, port);
+        sender.sendMessage(request, 2000);
+        System.out.println("Message sent.");
+    }
+
+    public void sendSyncWriteCommitMessage(String host, int port) {
+        SyncWriteCommitMessage msg = new SyncWriteCommitMessage();
+        sendMessage(host, port, msg);
+    }
+
+    public void sendSyncWriteSuggestion(String host, int port) {
+        SyncWriteSuggestionMessage msg = new SyncWriteSuggestionMessage();
+        sendMessage(host, port, msg);
+    }
+
+    public void sendSyncWriteSuggestionResponse(String host, int port) {
+        SyncWriteSuggestionMessage msg = new SyncWriteSuggestionMessage();
+        sendMessage(host, port, msg);
+    }
+}
