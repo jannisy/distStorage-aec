d41028c5617162c4489720a56e335122756f69f0
Jannis <janniskoch@posteo.de>
2016-05-24 20:29:50 +0200

    Add DistoApi

---
+++ src/main/java/de/tuberlin/aec/DistoNodeApi.java
@@ -0,0 +1,22 @@
+package de.tuberlin.aec;
+
+/**
+ * This class receives requests for a disto node.
+ * Its methods are called by the REST API.
+ * 
+ *
+ */
+public class DistoNodeApi {
+
+	public void put(String key, String value) {
+		
+	}
+	
+	public String get(String key) {
+		return "";
+	}
+	
+	public void delete(String key) {
+		
+	}
+}
